de71b39ad078214e1d80160b8adf34f781d0ca3a
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2020-11-07 12:40:19 +0300

    Flask terminé

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html'), 200
 
+@app.route("/hanokatra")
+def hanokatra():
+	return render_template('hanokatra.html'), 200
 
-@app.route("/admin")
-def administration():
-	return 'Tsy itako oo', 404
+@app.route("/mpandrindra")
+def mpandrindra():
+	return render_template('mpandrindra.html'), 200
+
+@app.route("/hamandrika")
+def hamandrika():
+	return render_template('hamandrika.html'), 200
+
+@app.route("/vokatra")
+def vokatra():
+	return render_template('vokatra.html'), 200
+
+@app.route("/mpanjifa")
+def mpanjifa():
+	return render_template('mpanjifa.html'), 200
 
 
 @app.route("/user/<int:username>")
